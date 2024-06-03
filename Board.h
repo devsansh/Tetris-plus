2ded4de1c8ba8bc88e5b4056ecab13c2d36019b7
Devansh Sharma <devsansh@yahoo.com>
2024-06-03 12:07:23 +0530

    Includes and Defines done

---
+++ Board.h
@@ -0,0 +1,17 @@
+#ifndef _BOARD_
+#define _BOARD_
+
+// *****---Includes---*****
+#include "Pieces.h"
+
+// *****---Defines---*****
+#define BOARD_LINE_WIDTH 6 //Width of each of 2 lines that delimit the board.
+#define BOARD_SIZE 16 // Width and Height of each block of a piece.
+#define BOARD_POSITION 320 // Center position of the board from the left of the screen.
+#define BOARD_WIDTH 10 // Board width in Blocks.
+#define BOARD_HEIGHT 10 // Board height in Blocks.
+#define MIN_VERTICAL_MARGIN 20 //Minimum vertical margin for the board limit.
+#define MIN_HORIZONTAL_MARGIN 20 //Minimum horizontal margin for the board limit.
+#define PIECE_BLOCKS 5 //Number of horizontal blocks of a matrix piece.
+
+

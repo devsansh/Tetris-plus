d355b9dcc545a692bdc52814563f9a02e8d5fd7c
Devansh Sharma <devsansh@yahoo.com>
2024-06-03 11:17:51 +0530

    Function calls done.

--- Pieces.cpp
+++ Pieces.cpp
@@ -276,3 +276,17 @@ int mPiecesInitialPosition[7][4][2] ={
         {-2,-3}
     },
 };
+
+int Pieces::GetBlockType(int pPiece, int pRotation, int pX, int pY){
+    return mPieces [pPiece][pRotation][pX][pY];
+}
+
+int Pieces::GetXInitialPosition (int pPiece, int pRotation)
+{
+	return mPiecesInitialPosition [pPiece][pRotation][0];
+}
+
+int Pieces::GetYInitialPosition (int pPiece, int pRotation)
+{
+	return mPiecesInitialPosition [pPiece][pRotation][1];
+}
